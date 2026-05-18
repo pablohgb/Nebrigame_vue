73c739c09bb83137886091385f6a6d678cb27ea5
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-05-18 19:44:57 +0200

    Comento las vueDevTools

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+//Comento las herramientas de desarollo de vue para la defensa del TFG
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
